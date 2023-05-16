2edb8f5bdcb9ba994e0b1703b1baba5834c4b1bd
welinton <wesgdeveloper@gmail.com>
2023-05-16 09:33:30 -0500

    se agrega solucion

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { useState } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
-      </div>
-    );
-  }
+function App() {
+  const [value, setValue] = useState(0); //estado para el tamaño del texto
+  const [text, setText] = useState(""); //estado para el texto
+
+  const handlerChange = (event) => {
+    setText(event.target.value);
+    setValue(event.target.value.length);
+  };
+
+  return (
+    <div className="container">
+      <textarea onChange={handlerChange} rows="3">
+        {text}
+      </textarea>
+      <div className="counter">{value}</div>
+    </div>
+  );
 }
 
 export default App;
